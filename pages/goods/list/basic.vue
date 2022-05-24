c70fcf66daeb1f388cde5651d393851d656ab599
hao7680 <yuhao_hwang@foxmail.com>
2022-05-24 09:31:44 +0800

    增加了默认SKU 去掉了下架时间

--- pages/goods/list/basic.vue
+++ pages/goods/list/basic.vue
@@ -17,17 +17,12 @@
             <el-input v-model.number="form.sort" type="number" min="0"></el-input>
             <div class="remark_txt">数字越小，排名越靠前</div>
           </el-form-item>
-          <el-form-item label="商品名称" prop="name" ref="name"
-            ><el-input placeholder="请输入商品名称" v-model="form.name"></el-input
-          ></el-form-item>
-          <el-form-item label="推广词"
-            ><el-input
-              type="textarea"
-              :autosize="{ minRows: 3 }"
-              v-model="form.name_pw"
-              placeholder="请输入推广词"
-            ></el-input
-          ></el-form-item>
+          <el-form-item label="商品名称" prop="name" ref="name">
+            <el-input placeholder="请输入商品名称" v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="推广词">
+            <el-input type="textarea" :autosize="{ minRows: 3 }" v-model="form.name_pw" placeholder="请输入推广词"></el-input>
+          </el-form-item>
         </div>
       </div>
       <div class="container add_edit">
@@ -42,15 +37,15 @@
             <div class="remark_txt">此分类将决定商品信息显示在热门推荐分类下</div>
           </el-form-item>
           <el-form-item label="商品标签">
-            <el-checkbox-group v-model="goodsDatas"
-              ><el-checkbox v-for="item in goodsTags" :label="item.name" :key="item._id"></el-checkbox
-            ></el-checkbox-group>
+            <el-checkbox-group v-model="goodsDatas">
+              <el-checkbox v-for="item in goodsTags" :label="item.name" :key="item._id"></el-checkbox>
+            </el-checkbox-group>
             <div class="remark_txt">此选项表示商品所属商品</div>
           </el-form-item>
           <el-form-item label="服务标签">
-            <el-checkbox-group v-model="serveDatas"
-              ><el-checkbox v-for="item in serveTags" :label="item.name" :key="item._id"></el-checkbox
-            ></el-checkbox-group>
+            <el-checkbox-group v-model="serveDatas">
+              <el-checkbox v-for="item in serveTags" :label="item.name" :key="item._id"></el-checkbox>
+            </el-checkbox-group>
             <div class="remark_txt">此选项表示商品对应的服务项</div>
           </el-form-item>
           <el-form-item label="商城价" prop="price" ref="price">
@@ -65,9 +60,9 @@
             <el-input v-model.number="form.limit" type="number" min="0"></el-input>
             <div class="remark_txt">0为不限购</div>
           </el-form-item>
-          <el-form-item label="库存" prop="stock_num" ref="stock_num"
-            ><el-input v-model.number="form.stock_num" type="number" min="0"></el-input
-          ></el-form-item>
+          <el-form-item label="库存" prop="stock_num" ref="stock_num">
+            <el-input v-model.number="form.stock_num" type="number" min="0"></el-input>
+          </el-form-item>
           <el-form-item label="已出售数">
             <el-input v-model.number="form.sale_cnt" type="number" min="0"></el-input>
             <el-checkbox class="w100" v-model.number="form.sale" :true-label="1" :false-label="0">显示</el-checkbox>
@@ -88,18 +83,6 @@
             <el-checkbox class="w100" v-model.number="form.share" :true-label="1" :false-label="0">显示</el-checkbox>
             <div class="remark_txt">勾选此选项将作分享销量</div>
           </el-form-item>
-          <el-form-item label="下架时间" prop="sold_out_time" ref="sold_out_time">
-            <el-date-picker
-              v-model="form.sold_out_time"
-              type="datetime"
-              placeholder="下架时间"
-              align="center"
-              class="time"
-              format="yyyy-MM-dd HH:mm:ss"
-              value-format="yyyy-MM-dd HH:mm:ss"
-              :picker-options="pickerOptions"
-            ></el-date-picker>
-          </el-form-item>
           <el-form-item label="状态">
             <el-radio-group v-model="form.state">
               <el-radio label="待审核">待审核</el-radio>
@@ -114,153 +97,145 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        categoryOptions: [],
-        goodsDatas: [],
-        serveDatas: [],
-        goodsTags: [],
-        serveTags: [],
-        form: {
-          cid: '',
-          cids: [],
-          sort: 0,
-          name: '',
-          name_pw: '',
-          limited: 0,
-          hot: 0,
-          tags: [],
-          price: 0,
-          market_price: 0,
-          limit: 0,
-          stock_num: 0,
-          sale_cnt: 0,
-          sale: 0,
-          visit_cnt: 0,
-          visit: 0,
-          collect_cnt: 0,
-          collect: 0,
-          share_cnt: 0,
-          share: 0,
-          sold_out_time: '',
-          state: '待审核',
+export default {
+  data() {
+    return {
+      categoryOptions: [],
+      goodsDatas: [],
+      serveDatas: [],
+      goodsTags: [],
+      serveTags: [],
+      form: {
+        cid: '',
+        cids: [],
+        sort: 0,
+        name: '',
+        name_pw: '',
+        limited: 0,
+        hot: 0,
+        tags: [],
+        price: 0,
+        market_price: 0,
+        limit: 0,
+        stock_num: 0,
+        sale_cnt: 0,
+        sale: 0,
+        visit_cnt: 0,
+        visit: 0,
+        collect_cnt: 0,
+        collect: 0,
+        share_cnt: 0,
+        share: 0,
+        state: '待审核',
+      },
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() < Date.now()
         },
-        pickerOptions: {
-          disabledDate(time) {
-            return time.getTime() < Date.now();
+      },
+      rules: {
+        cid: [
+          {
+            required: true,
+            message: '请选择平台分类',
+            trigger: 'change',
           },
-        },
-        rules: {
-          cid: [
-            {
-              required: true,
-              message: '请选择平台分类',
-              trigger: 'change',
-            },
-          ],
-          name: [
-            {
-              required: true,
-              message: '请输入商品名称',
-              trigger: 'change',
-            },
-          ],
-          price: [
-            {
-              required: true,
-              message: '请输入商城价，必须大于0',
-              trigger: 'change',
-            },
-          ],
-          market_price: [
-            {
-              required: true,
-              message: '请输入市场价，必须大于0',
-              trigger: 'change',
-            },
-          ],
-          stock_num: [
-            {
-              required: true,
-              message: '请输入库存，必须大于0',
-              trigger: 'change',
-            },
-          ],
-          limit: [
-            {
-              required: true,
-              message: '请输入库存，必须大于0',
-              trigger: 'change',
-            },
-          ],
-          sold_out_time: [
-            {
-              required: true,
-              message: '请选择下架时间',
-              trigger: 'change',
-            },
-          ],
-        },
-      };
-    },
-    methods: {
-      setChildren(res) {
-        res.forEach((item) => {
-          if (item.children && item.children.length <= 0) {
-            item.children = null;
-            return;
-          }
-
-          this.setChildren(item.children);
-        });
+        ],
+        name: [
+          {
+            required: true,
+            message: '请输入商品名称',
+            trigger: 'change',
+          },
+        ],
+        price: [
+          {
+            required: true,
+            message: '请输入商城价，必须大于0',
+            trigger: 'change',
+          },
+        ],
+        market_price: [
+          {
+            required: true,
+            message: '请输入市场价，必须大于0',
+            trigger: 'change',
+          },
+        ],
+        stock_num: [
+          {
+            required: true,
+            message: '请输入库存，必须大于0',
+            trigger: 'change',
+          },
+        ],
+        limit: [
+          {
+            required: true,
+            message: '请输入库存，必须大于0',
+            trigger: 'change',
+          },
+        ],
       },
-      setBaseData(res) {
-        this.setChildren(res[0].datas);
-        this.categoryOptions = res[0].datas;
-
-        this.goodsTags = res[1].datas.filter((x) => x.type == '商品');
-        this.serveTags = res[1].datas.filter((x) => x.type == '服务');
-      },
-      submitData(callback) {
-        this.$refs['ruleForm'].validate((valid, obj) => {
-          this.$api.set_unvalidated_form_focus(this, obj);
-          if (valid) {
-            this.form.tags = [...this.goodsDatas, ...this.serveDatas];
-
-            let cascaderObj = this.$refs.cascaderObj.getCheckedNodes();
-            this.form.cids = cascaderObj[0].path;
-
-            if (typeof callback === 'function') {
-              callback(this.form, true);
-            }
-          } else {
-            if (typeof callback === 'function') {
-              callback(this.form, false);
-            }
-            return false;
-          }
-        });
-      },
-      getData(callback) {
-        this.submitData(callback);
-      },
-      setData(data) {
-        data.tags = data.tags || [];
-        data.cids = data.cids || [];
-
-        this.goodsDatas = data.tags.filter((x) => this.goodsTags.find((t) => t.name == x));
-        this.serveDatas = data.tags.filter((x) => this.serveTags.find((t) => t.name == x));
-        this.form.cid = data.cid;
-
-        for (const key in this.form) {
-          if (this.form.hasOwnProperty(key) && key != 'cid') {
-            this.form[key] = data[key];
-          }
+    }
+  },
+  methods: {
+    setChildren(res) {
+      res.forEach(item => {
+        if (item.children && item.children.length <= 0) {
+          item.children = null
+          return
         }
-      },
+
+        this.setChildren(item.children)
+      })
     },
-  };
+    setBaseData(res) {
+      this.setChildren(res[0].datas)
+      this.categoryOptions = res[0].datas
+
+      this.goodsTags = res[1].datas.filter(x => x.type == '商品')
+      this.serveTags = res[1].datas.filter(x => x.type == '服务')
+    },
+    submitData(callback) {
+      this.$refs['ruleForm'].validate((valid, obj) => {
+        this.$api.set_unvalidated_form_focus(this, obj)
+        if (valid) {
+          this.form.tags = [...this.goodsDatas, ...this.serveDatas]
+
+          let cascaderObj = this.$refs.cascaderObj.getCheckedNodes()
+          this.form.cids = cascaderObj[0].path
+
+          if (typeof callback === 'function') {
+            callback(this.form, true)
+          }
+        } else {
+          if (typeof callback === 'function') {
+            callback(this.form, false)
+          }
+          return false
+        }
+      })
+    },
+    getData(callback) {
+      this.submitData(callback)
+    },
+    setData(data) {
+      data.tags = data.tags || []
+      data.cids = data.cids || []
+
+      this.goodsDatas = data.tags.filter(x => this.goodsTags.find(t => t.name == x))
+      this.serveDatas = data.tags.filter(x => this.serveTags.find(t => t.name == x))
+      this.form.cid = data.cid
+
+      for (const key in this.form) {
+        if (this.form.hasOwnProperty(key) && key != 'cid') {
+          this.form[key] = data[key]
+        }
+      }
+    },
+  },
+}
 </script>
 
 <style></style>
--- pages/goods/list/sku.vue
+++ pages/goods/list/sku.vue
@@ -5,12 +5,7 @@
         <h3 class="margin_b_20">SKU</h3>
         <div>
           <el-form-item label="开启SKU">
-            <el-switch
-              @change="open_switch_sku"
-              v-model="switch_sku"
-              active-color="#feaa30"
-              inactive-color="#dcdfe6"
-            ></el-switch>
+            <el-switch @change="open_switch_sku" v-model="switch_sku" active-color="#feaa30" inactive-color="#dcdfe6"></el-switch>
           </el-form-item>
           <el-form-item label="编辑SKU" v-if="switch_sku">
             <div>
@@ -37,9 +32,7 @@
                 @keyup.enter.native="handleInputConfirm_spec"
                 @blur="handleInputConfirm_spec"
               ></el-input>
-              <el-button :disabled="!checked_spec" v-else class="button-new-tag" size="small" @click="showInput_spec"
-                >添加</el-button
-              >
+              <el-button :disabled="!checked_spec" v-else class="button-new-tag" size="small" @click="showInput_spec">添加</el-button>
             </div>
           </el-form-item>
           <el-form-item v-if="switch_sku">
@@ -64,193 +57,190 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        switch_sku: true,
-        skus: [],
-        goods: {},
-        dynamicTags_spec: [],
-        inputVisible_spec: false,
-        inputValue_spec: '',
-        checked_spec: true,
-        spec_s: '规格',
-        tableData: [],
-        tableSkuData: [],
-      };
-    },
-    watch: {
-      checked_spec: function (arrs) {
-        for (var i = 0; i < arrs.length; i++) {
-          for (var j = i + 1; j < arrs.length; j++) {
-            if (arrs[i] == arrs[j]) {
-              //第一个等同于第二个，splice方法删除第二个
-              arrs.splice(j, 1);
-              j--;
-            }
+export default {
+  data() {
+    return {
+      switch_sku: true,
+      skus: [],
+      goods: {},
+      dynamicTags_spec: ['电子版(个人授权)', '纸质版(个人授权)', '电子版(商业授权)', '纸质版(商业授权)'],
+      inputVisible_spec: false,
+      inputValue_spec: '',
+      checked_spec: true,
+      spec_s: '规格',
+      tableData: [],
+      tableSkuData: [],
+    }
+  },
+  watch: {
+    checked_spec: function(arrs) {
+      for (var i = 0; i < arrs.length; i++) {
+        for (var j = i + 1; j < arrs.length; j++) {
+          if (arrs[i] == arrs[j]) {
+            //第一个等同于第二个，splice方法删除第二个
+            arrs.splice(j, 1)
+            j--
           }
         }
-      },
+      }
     },
-    methods: {
-      submitData(callback) {
-        this.skus = [];
+  },
+  created() {
+    this.affirm()
+  },
+  methods: {
+    submitData(callback) {
+      this.skus = []
 
-        this.tableSkuData.forEach((x) => {
-          this.skus.push(x);
-        });
+      this.tableSkuData.forEach(x => {
+        this.skus.push(x)
+      })
 
-        if (typeof callback === 'function') {
-          callback(
-            this.skus,
-            JSON.stringify({
-              spec_s: this.spec_s,
-              spec: this.dynamicTags_spec,
+      if (typeof callback === 'function') {
+        callback(
+          this.skus,
+          JSON.stringify({
+            spec_s: this.spec_s,
+            spec: this.dynamicTags_spec,
+          })
+        )
+      }
+    },
+    getData(callback) {
+      this.submitData(callback)
+    },
+    setData(res) {
+      this.form = res.goods
+      let skus = JSON.parse(this.form.skus)
+      this.spec_s = skus.spec_s
+      this.tableSkuData = res.goods_sku
+      this.dynamicTags_spec = skus.spec
+      this.checked_spec = this.dynamicTags_spec.length > 0
+    },
+    setGoods(res) {
+      this.goods = res
+    },
+    affirm() {
+      console.log(123)
+      // 规格
+      if (this.dynamicTags_spec.length > 0) {
+        this.dynamicTags_spec.forEach((spec, spec_idx) => {
+          if (!this.tableSkuData.find(x => x.spec == spec))
+            this.tableSkuData.push({
+              price: this.goods.price,
+              stock_num: this.goods.stock_num,
+              spec: spec,
+              spec_s: spec_idx,
             })
-          );
-        }
-      },
-      getData(callback) {
-        this.submitData(callback);
-      },
-      setData(res) {
-        this.form = res.goods;
-
-        let skus = JSON.parse(this.form.skus);
-        this.spec_s = skus.spec_s;
-
-        this.tableSkuData = res.goods_sku;
-
-        this.dynamicTags_spec = skus.spec;
-
-        this.checked_spec = this.dynamicTags_spec.length > 0;
-      },
-      setGoods(res) {
-        this.goods = res;
-      },
-      affirm() {
-        // 规格
-        if (this.dynamicTags_spec.length > 0) {
-          this.dynamicTags_spec.forEach((spec, spec_idx) => {
-            if (!this.tableSkuData.find((x) => x.spec == spec))
-              this.tableSkuData.push({
-                price: this.goods.price,
-                stock_num: this.goods.stock_num,
-                spec: spec,
-                spec_s: spec_idx,
-              });
-          });
-
-          this.tableSkuData = this.tableSkuData.filter((x) => this.dynamicTags_spec.find((s) => s == x.spec));
-          console.log('this.tableSkuData', this.tableSkuData);
-        }
-      },
-      handleClose_spec(tag) {
-        this.dynamicTags_spec.splice(this.dynamicTags_spec.indexOf(tag), 1);
-        this.tableSkuData.splice(
-          this.tableSkuData.findIndex((x) => x.spec == tag),
-          1
-        );
-      },
-      showInput_spec() {
-        this.inputVisible_spec = true;
-        this.$nextTick(() => {
-          this.$refs.saveTagInput_spec.$refs.input.focus();
-        });
-      },
-      handleInputConfirm_spec() {
-        if (this.inputValue_spec) {
-          if (this.dynamicTags_spec.find((x) => x == this.inputValue_spec)) {
-            this.$message({
-              message: this.inputValue_spec + '已存在',
-              type: 'warning',
-            });
-            return;
-          }
-          this.dynamicTags_spec.push(this.inputValue_spec);
-          this.affirm();
-        }
-        this.inputVisible_spec = false;
-        this.inputValue_spec = '';
-      },
-      open_spec() {
-        if (this.checked_spec) return;
-        this.$confirm(`此操作将清除【${this.spec_s}】数据, 是否继续?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning',
         })
-          .then(() => {
-            this.$message({
-              type: 'success',
-              message: '清除成功!',
-            });
-            this.dynamicTags_spec = [];
-            this.tableSkuData = [];
-          })
-          .catch(() => {
-            this.checked_spec = true;
-          });
-      },
-      open_switch_sku() {
-        if (this.switch_sku) return;
-        this.$confirm(`此操作将清除数据, 是否继续?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning',
-        })
-          .then(() => {
-            this.$message({
-              type: 'success',
-              message: '清除成功!',
-            });
-            this.dynamicTags_spec = [];
-            this.tableData = [];
-            this.tableSkuData = [];
-          })
-          .catch(() => {
-            this.switch_sku = true;
-          });
-      },
+        this.tableSkuData = this.tableSkuData.filter(x => this.dynamicTags_spec.find(s => s == x.spec))
+        console.log('this.tableSkuData', this.tableSkuData)
+      }
     },
-  };
+    handleClose_spec(tag) {
+      this.dynamicTags_spec.splice(this.dynamicTags_spec.indexOf(tag), 1)
+      this.tableSkuData.splice(this.tableSkuData.findIndex(x => x.spec == tag), 1)
+    },
+    showInput_spec() {
+      this.inputVisible_spec = true
+      this.$nextTick(() => {
+        this.$refs.saveTagInput_spec.$refs.input.focus()
+      })
+    },
+    handleInputConfirm_spec() {
+      if (this.inputValue_spec) {
+        if (this.dynamicTags_spec.find(x => x == this.inputValue_spec)) {
+          this.$message({
+            message: this.inputValue_spec + '已存在',
+            type: 'warning',
+          })
+          return
+        }
+        this.dynamicTags_spec.push(this.inputValue_spec)
+        this.affirm()
+      }
+      this.inputVisible_spec = false
+      this.inputValue_spec = ''
+    },
+    open_spec() {
+      if (this.checked_spec) return
+      this.$confirm(`此操作将清除【${this.spec_s}】数据, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '清除成功!',
+          })
+          this.dynamicTags_spec = []
+          this.tableSkuData = []
+        })
+        .catch(() => {
+          this.checked_spec = true
+        })
+    },
+    open_switch_sku() {
+      if (this.switch_sku) return
+      this.$confirm(`此操作将清除数据, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '清除成功!',
+          })
+          this.dynamicTags_spec = []
+          this.tableData = []
+          this.tableSkuData = []
+        })
+        .catch(() => {
+          this.switch_sku = true
+        })
+    },
+  },
+}
 </script>
 
 <style>
-  input[type="file"] {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 99;
-    width: 100%;
-    cursor: pointer;
-    opacity: 0;
-  }
+input[type='file'] {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
+  width: 100%;
+  cursor: pointer;
+  opacity: 0;
+}
 </style>
 
 <style>
-  .tag-new-tag {
-    margin-left: 24px;
-  }
+.tag-new-tag {
+  margin-left: 24px;
+}
 
-  .button-new-tag, .input-new-tag {
-    height: 32px;
-    margin-top: 0;
-    margin-left: 24px;
-  }
+.button-new-tag,
+.input-new-tag {
+  height: 32px;
+  margin-top: 0;
+  margin-left: 24px;
+}
 
-  .input-new-tag {
-    width: 100px;
-    line-height: 32px;
-  }
+.input-new-tag {
+  width: 100px;
+  line-height: 32px;
+}
 
-  >>> .input-new-tag .el-input__inner {
-    height: 100%;
-  }
+>>> .input-new-tag .el-input__inner {
+  height: 100%;
+}
 
-  .el-button--small {
-    border: 1px solid #dcdfe6;
-  }
+.el-button--small {
+  border: 1px solid #dcdfe6;
+}
 </style>
